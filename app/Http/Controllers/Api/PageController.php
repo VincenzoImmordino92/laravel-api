19d149da75c0c2d4d436eeaff8e28f2b1ea2f3af
vincenzoimmordino92 <vincenzoimmordino92@gmail.com>
2023-12-12 17:22:04 +0100

    [fix] pageController

--- app/Http/Controllers/Api/PageController.php
+++ app/Http/Controllers/Api/PageController.php
@@ -33,15 +33,15 @@ class PageController extends Controller
 
         if($project) $success = true;
         else $success = false;
-
-        if($project->image){
-            //se è presente l'immagine con il etodo asset ricavo l'url assoluto
-            $project->image = asset('storage/'.$project->image);
-        }else{
-            //se l'immagine non è presente restiutisce il placeholder cioè l'immagine vuota
-            $project->image = asset('storage/uploads/placeholder.png');
+        if($success){
+            if($project->image){
+                //se è presente l'immagine con il etodo asset ricavo l'url assoluto
+                $project->image = asset('storage/'.$project->image);
+            }else{
+                //se l'immagine non è presente restiutisce il placeholder cioè l'immagine vuota
+                $project->image = asset('storage/uploads/placeholder.png');
+            }
         }
-
         return response()->json(compact('project','success'));
     }
 
